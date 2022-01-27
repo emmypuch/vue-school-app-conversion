b36f0ba74b5cf39e35d0de422effdef7c4213f50
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2022-01-27 21:44:50 +0100

    created a new component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <div id="app" class="bg-blue-500">hello</div>
+  <div id="app">
+    <home />
+  </div>
 </template>
 
 <script>
+import Home from "./components/Home.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Home,
+  },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,49 @@
 <template>
-  <div></div>
+  <div id="navLinks">
+    <header class="flex">
+      <div class="headerLogo">
+        <h1>Logo</h1>
+        <!-- <a href="/"><img :src="Image" alt="" /></a> -->
+      </div>
+      <div class="navbar">
+        <div class="menu-lists">
+          <router-link to="/about" id="listItems">Courses</router-link>
+          <a href="/#projects" id="listItems">Workshops</a>
+          <a href="/#contact" id="listItems">Plans</a>
+          <a href="/#contact" id="listItemsLink">Login</a>
+        </div>
+        <!-- <div class="hamburger">
+          <span class="line line1"></span>
+          <span class="line line2"></span>
+          <span class="line line3"></span>
+        </div> -->
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
-export default {};
+// const Image = require("../assets/images/headerLogo.png");
+export default {
+  name: "Header",
+  data() {
+    return {};
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  overflow: hidden;
+}
+
+section#header {
+  background-color: #28335d;
+}
+</style>
