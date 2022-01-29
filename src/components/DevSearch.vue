b006fa06d40fcdda6fa6d6e31421ec2dd8b42363
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2022-01-29 21:48:44 +0100

    setup

--- src/components/DevSearch.vue
+++ src/components/DevSearch.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="pl-24 pr-24 pt-32 flex justify-center items-center">
     <h1 class="text-center text-4xl font-bold">{{ msg }}</h1>
+    <div class="dev-search-numbers-wrapper relative">
+      <div class="number-container absolute"></div>
+      <div
+        class="dev-search-card-container flex justify-between bg-red-500 p-6"
+      >
+        <div class="card-image">
+          <img src="" alt="" />
+        </div>
+        <div class="card-content">
+          <h3>Create Account</h3>
+          <p>Gain access to our data base of 100’s of Vue developers</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
