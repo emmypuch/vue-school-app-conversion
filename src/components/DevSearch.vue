8fce0673594ca04c0749c09e437c7606e36e2061
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2022-01-29 21:34:37 +0100

    modified devsearch component

--- src/components/DevSearch.vue
+++ src/components/DevSearch.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="pl-24 pr-24 pt-32">hello</div>
+  <div class="pl-24 pr-24 pt-32 flex justify-center items-center">
+    <h1 class="text-center text-4xl font-bold">{{ msg }}</h1>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      msg: "How Dev Search Works",
+    };
+  },
+};
 </script>
 
 <style scoped>
 div {
   background-color: #fbfcff;
+}
+
+h1 {
+  color: #242d55;
 }
 </style>
