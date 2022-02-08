02aa98c737c0aa7d20ddeb181ccba8c6d63c2df7
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2022-02-08 20:37:30 +0100

    added content

--- src/components/DevSearch.vue
+++ src/components/DevSearch.vue
@@ -5,57 +5,62 @@
       <div class="number-container rounded-xl"></div>
     </div>
     <div
-      class="dev-search-card-container flex bg-white shadow-lg rounded-sm p-6 mt-16"
+      class="dev-search-card-container flex bg-white shadow-lg rounded-sm p-8 mt-16"
     >
       <div class="card-image">
-        <img :src="Image" alt="dev" class="" />
+        <img :src="ImagePen" alt="dev" class="h-20" />
       </div>
       <div class="card-content ml-6">
         <h3 class="text-2xl font-bold">Create Account</h3>
         <p class="mt-2 text-base font-medium">
-          Gain access to our data base of 100’s of Vue developers
+          Register and subscribe to get secure access to our developer database.
         </p>
       </div>
     </div>
 
     <div
-      class="dev-search-card-container flex bg-white shadow-lg rounded-sm p-6 mt-10"
+      class="dev-search-card-container flex bg-white shadow-lg rounded-sm p-8 mt-10"
     >
       <div class="card-image">
-        <img :src="Image" alt="dev" class="" />
+        <img :src="ImageSearch" alt="dev" class="h-20" />
       </div>
       <div class="card-content ml-6">
-        <h3 class="text-2xl font-bold">Create Account</h3>
+        <h3 class="text-2xl font-bold">Search for your ideal Vue developer</h3>
         <p class="mt-2 text-base font-medium">
-          Gain access to our data base of 100’s of Vue developers
+          Utilise our easy-to-use interface to select the exact criteria for
+          your ideal candidate. Search options include location, skillsets,
+          experience and more.
         </p>
       </div>
     </div>
 
     <div
-      class="dev-search-card-container flex bg-white shadow-lg rounded-sm p-6 mt-10"
+      class="dev-search-card-container flex bg-white shadow-lg rounded-sm p-8 mt-10"
     >
       <div class="card-image">
-        <img :src="Image" alt="dev" class="" />
+        <img :src="ImageEnvelop" alt="dev" class="h-20" />
       </div>
       <div class="card-content ml-6">
-        <h3 class="text-2xl font-bold">Create Account</h3>
+        <h3 class="text-2xl font-bold">Connect directly with your candidate</h3>
         <p class="mt-2 text-base font-medium">
-          Gain access to our data base of 100’s of Vue developers
+          Browse the list of developers and connect with the candidates that fit
+          your criteria
         </p>
       </div>
     </div>
 
     <div
-      class="dev-search-card-container flex bg-white shadow-lg rounded-sm p-6 mt-10"
+      class="dev-search-card-container flex bg-white shadow-lg rounded-sm p-8 mt-10"
     >
       <div class="card-image">
-        <img :src="Image" alt="dev" class="" />
+        <img :src="ImageArrow" alt="dev" class="h-20" />
       </div>
       <div class="card-content ml-6">
-        <h3 class="text-2xl font-bold">Create Account</h3>
+        <h3 class="text-2xl font-bold">Built to scale to your needs</h3>
         <p class="mt-2 text-base font-medium">
-          Gain access to our data base of 100’s of Vue developers
+          Each subscription includes your ability to connect with up to 50
+          developers per month on this introductory offer, with the option to
+          purchase as many additional connections as you need!
         </p>
       </div>
     </div>
@@ -63,12 +68,18 @@
 </template>
 
 <script>
-const Image = require("../assets/images/devs.svg");
+const ImagePen = require("../assets/images/pen.png");
+const ImageSearch = require("../assets/images/search.png");
+const ImageEnvelop = require("../assets/images/envelop.png");
+const ImageArrow = require("../assets/images/arrow.png");
 export default {
   data() {
     return {
       msg: "How Dev Search Works",
-      Image,
+      ImagePen,
+      ImageSearch,
+      ImageEnvelop,
+      ImageArrow,
     };
   },
 };
