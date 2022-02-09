df66c14d6c78a1327877d016c1e8db687e3ad6ea
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2022-02-09 16:42:09 +0100

    updated

--- src/components/ExperienceDev.vue
+++ src/components/ExperienceDev.vue
@@ -3,21 +3,17 @@
     <h1 class="text-center text-4xl font-bold mt-12 text-white">{{ msg }}</h1>
     <div class="experienced-wrapper flex mt-14">
       <div class="card px-12 py-8 rounded-md w-full">
-        <h3 class="text-xl font-bold text-white">
-          Average Front End Experience
-        </h3>
+        <h3 class="text-xl font-bold">Average Front End Experience</h3>
         <h1 class="text-4xl font-extrabold text-white mt-2">5+ Years</h1>
       </div>
 
       <div class="card px-12 py-8 rounded-md ml-8 w-full">
-        <h3 class="text-xl font-bold text-white">Average Vue.js Experience</h3>
+        <h3 class="text-xl font-bold">Average Vue.js Experience</h3>
         <h1 class="text-4xl font-extrabold text-white mt-2">2+ Years</h1>
       </div>
 
       <div class="card px-10 py-8 rounded-md ml-8 w-full">
-        <h3 class="text-xl font-bold text-white">
-          Average Technology Experience
-        </h3>
+        <h3 class="text-xl font-bold">Average Technology Experience</h3>
         <h1 class="text-4xl font-extrabold text-white mt-2">9+ Years</h1>
       </div>
     </div>
@@ -41,5 +37,9 @@ div.experienced-wrapper {
 
 div.card {
   background-color: #2a3565;
+}
+
+h3 {
+  color: #c5c7d3;
 }
 </style>
