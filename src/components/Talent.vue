047a9e5bddefc43c372683cb3598ef8a89a08009
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2022-02-21 14:22:30 +0100

    cleaned code

--- src/components/Talent.vue
+++ src/components/Talent.vue
@@ -3,18 +3,16 @@
     <!-- Hire Talent Section Begins -->
     <section id="hire-talent-container" class="">
       <div
-        class="hire-talent-flex md:flex-col md:justify-center md:items-center lg:flex lg:justify-between lg:pl-20 lg:pr-20 lg:pt-20"
+        class="hire-talent-flex lg:flex lg:justify-between lg:pl-20 lg:pr-20 lg:pt-20"
       >
         <div class="hire-talent-description">
           <h1
-            class="md:text-lg md:pl-0 md:text-center lg:text-6xl font-extrabold text-white lg:pl-8 leading-tight"
+            class="lg:text-6xl font-extrabold text-white lg:pl-8 leading-tight"
           >
             Hire the Best <br />
             Vue.js talent
           </h1>
-          <p
-            class="lg:text-base lg:pl-10 lg:mt-8 md:pl-0 md:mt-4 md:text-center font-semibold"
-          >
+          <p class="lg:text-base lg:pl-10 lg:mt-8 font-semibold">
             Engage with the Vue School community of experienced Vue.js
           </p>
           <p class="text2 pl-10 font-semibold md:text-center">
@@ -22,9 +20,7 @@
           </p>
           <Button />
         </div>
-        <div
-          class="hire-talent-description lg:pr-12 md:pr-0 md:flex md:justify-center md:items-center"
-        >
+        <div class="hire-talent-description lg:pr-12">
           <img :src="ImageContent" alt="Vue School Logo" class="max-w-full" />
         </div>
       </div>
